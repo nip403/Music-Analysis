095c5ca03393ec69374b6b221fd2a4ac081e0366
Nip403 <francoisjiayan@gmail.com>
2024-03-29 16:54:43 -0400

    Packaged

--- tests/bohemian.py
+++ tests/bohemian.py
@@ -2,9 +2,7 @@ import sys
 import os
 import numpy as np
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+sys.path.append(parent_dir := os.getcwd() + "\\src")
 
 from MusicInsights.Waveform import Waveform
 from MusicInsights.utils import playback, visualise, to_json
@@ -14,14 +12,12 @@ np.set_printoptions(threshold=np.inf)
 rate = 8000
 duration = 5
 
-
-"""
-print([p for p, i in enumerate(dev) if i > 0.1])
-print(bohemian.beat_frames)
-#print(dev[np.array([[i-1, i, i+1] for i in bohemian.beat_frames]).flatten()])
-"""
-
 if __name__ == "__main__":
     bohemian = Waveform(fp=parent_dir + "\\music\\Bohemian Rhapsody.mp3", sample_rate=rate, duration=duration)
+
+    dev = bohemian.deviation[:]
+    print([p for p, i in enumerate(dev) if i > 0.1]) # significant samples
+    print(bohemian.beat_frames) # beats
+    print(dev[np.array([[i-1, i, i+1] for i in bohemian.beat_frames]).flatten()]) # sample "energy" at beat frame edges
 
     to_json(bohemian, os.path.dirname(__file__))
